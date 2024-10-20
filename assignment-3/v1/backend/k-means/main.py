c04f27cc50875686ebd513e16136b213f72ff7ad
MatthewLeigh <matthewcross97@gmail.com>
2024-10-20 13:18:46 +1100

    Return cluster statistics with k-means prediction

--- assignment-3/v1/backend/k-means/main.py
+++ assignment-3/v1/backend/k-means/main.py
@@ -3,25 +3,43 @@ from pydantic import BaseModel
 import numpy as np
 from model import KMeansModel
 
+# Create a FastAPI app
 app = FastAPI()
+
+# Load the KMeansModel
 kmeans_model = KMeansModel()
 
+# Define the input data model
 class PollutantData(BaseModel):
-    pollutant_co: float
-    pollutant_no: float
-    pollutant_no2: float
-    pollutant_ozone: float
-    pollutant_pm10: float
-    pollutant_s02: float
+    CO_ppm: float
+    NO_pphm: float
+    NO2_pphm: float
+    OZONE_pphm: float
+    PM10_ug_m3: float
+    SO2_pphm: float
+    label: str
 
+# Define the predict endpoint for K-Means
 @app.post("/predict")
 async def predict_pollutant_cluster(data: PollutantData):
     try:
-        new_data = np.array([[data.pollutant_co, data.pollutant_no, 
-                              data.pollutant_no2, data.pollutant_ozone, 
-                              data.pollutant_pm10, data.pollutant_s02]])
-        predicted_cluster = kmeans_model.predict(new_data)
-        return {"predicted_cluster": int(predicted_cluster[0])}
+
+        # Get the new values to predict
+        new_data = np.array([[data.CO_ppm, data.NO_pphm, data.NO2_pphm, data.OZONE_pphm, 
+                              data.PM10_ug_m3, data.SO2_pphm]])
+
+        # Predict the cluster
+        predicted_cluster = kmeans_model.predict(new_data)[0]
+
+        # Get the statistics for the predicted cluster and label
+        label_stats = kmeans_model.get_cluster_statistics(predicted_cluster, data.label)
+        
+        # Return the predicted cluster and label statistics
+        return {
+            "predicted_cluster": int(predicted_cluster),
+            "label_stats": label_stats
+        }
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
